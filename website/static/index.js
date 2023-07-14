7bb8b8623933323de26fa8211fce04aba05c0034
DeskPaSeat0 <jeremyarquiza@gmail.com>
2023-07-14 13:59:38 +0800

    First commit

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+function deleteNode(noteId)
+{
+    fetch("/delete-note", {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId })
+    }).then((_res) => {
+        window.location.href = "/";
+    }); 
+}
